38eed6b56c1061feb65fc3520af05702b68fcc8c
Shuhrat <xolmatov804@gmail.com>
2022-10-16 10:48:33 +0500

    test1

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-def salom():
-    pass
+import sqlite3
+def add_user_id(user_id):
+    db=sqlite3.connect("datas.db")
+    cursor=db.cursor()
+    cursor.execute("""CREAT TABLE IF NOT EXISTS users (
+    user_id INTEGER
+    )""")
+    db.commit()
+    cursor.execute(f"INSERT INTO users VALUES ({user_id})")
+    db.commit()
+    db.close()
